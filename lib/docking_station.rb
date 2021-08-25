19f60b3418b9e7aca5bcae19746d746f41c5d88e
Sophie Bonneval <maldorana@gmail.com>
2021-08-25 16:01:20 +0100

    add dock method and release bike exception

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,16 @@
-require_relative "bike"
-
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail "No bikes available" unless @bike
+    @bike
   end
+
+  def dock(bike)
+    fail "Docking station full" if @bike
+    @bike = bike
+  end
+
 end 
 
 #.release_bike to give Bike.new object
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,35 @@
 require "docking_station"
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-  it "gets a bike" do
-    bike = DockingStation.new.release_bike
-    expect(bike).to eq(bike)
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike) }
+    it "release a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
+
+    it "doesn't release a bike because it doesn't exist" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
+  end 
+
+  describe "#dock" do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it "docks bike" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq(bike) 
+    end
+    it "raises an error when full" do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
+    end
+  end
+
+  it { is_expected.to respond_to(:bike) }
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
   end
 end
